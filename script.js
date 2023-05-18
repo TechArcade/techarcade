f634231e2e5c7e0eaeb6b3c22cc4fb2a1d14aa03
bruno <brunoregis1999@gmail.com>
2023-05-17 22:11:33 -0300

    s

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
+$("#app").load("/template/nav.html");
 
+function getTextFilesFromFolder() {
+  const folderPath = "posts/";
+  const textFiles = [];
 
+  $.ajax({
+    url: folderPath,
+    success: function (data) {
+      $(data)
+        .find("a")
+        .each(function () {
+          const href = $(this).attr("href");
+          if (href.endsWith(".txt")) {
+            const filePath = href;
 
-$("#y").load("/template/nav.html");
+            // Fetch the text file
+            $.get(filePath, function (text) {
+              textFiles.push(text);
+            });
+          }
+        });
+    },
+    error: function (error) {
+      console.error("Error loading folder:", error);
+    },
+  });
 
+  return textFiles;
+}
 
+function loadPosts() {
+  $("#posts").html(markdown(ein.value));
+}
 
-
-
-
+getTextFilesFromFolder();
